778d7a928ac5189e7e943e260e9efe0bcc2bc45c
Zac Gibson <zgibson@live.com>
2020-06-30 17:13:05 -0500

    add settings and layout changes depending on token

--- src/popup/App.js
+++ src/popup/App.js
@@ -3,29 +3,44 @@ import * as browser from 'webextension-polyfill';
 import { view } from '@risingstack/react-easy-state';
 
 import SearchPage from './components/searchPage';
+import SettingsPage from './components/settingsPage';
 import PlexLogin from './components/plexLogin';
+import TopBar from './components/topBar';
 
 import './App.css';
 import dataStore from './dataStore';
 
 function App() {
   // check if we have a token stored and tell the view not to show login screen
-  const storageItem = browser.storage.local.get('token');
+  const storageItem = browser.storage.local.get(['token']);
   storageItem.then(res => {
-    if (res.token != '' && res.token != null) {
+    const { token } = res;
+    if (token != '' && token != null) {
       dataStore.plex_authenticated = true;
     } else {
       dataStore.plex_authenticated = false;
     }
   });
 
-  const { plex_authenticated } = dataStore;
-  console.log(plex_authenticated);
+  const { plex_authenticated, settings_view } = dataStore;
   return (
-    <div className="App">
-      <header className="App-header">
-        {plex_authenticated ? <SearchPage /> : <PlexLogin />}
-      </header>
+    <div className="app">
+      {plex_authenticated ? (
+        <div className="top">
+          <TopBar />
+        </div>
+      ) : null}
+      <div className="body">
+        {plex_authenticated ? (
+          !settings_view ? (
+            <SearchPage />
+          ) : (
+            <SettingsPage />
+          )
+        ) : (
+          <PlexLogin />
+        )}
+      </div>
     </div>
   );
 }
