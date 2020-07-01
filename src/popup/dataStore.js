069a2623fd619ff3ea1b5db711019f779d4c8672
Zac Gibson <zgibson@live.com>
2020-07-01 16:37:47 -0500

    slots for more dataStores and some comments

--- src/popup/dataStore.js
+++ src/popup/dataStore.js
@@ -1,10 +1,12 @@
 import { store } from '@risingstack/react-easy-state';
 
 const dataStore = store({
-  plex_authenticated: true,
+  plex_authenticated: true, // whether a token is saved or not (we dont keep it here tho)
   settings_view: false, // whether user is shown settings view or not
-  selected_server_uri: null,
-  server_options: [],
+  selected_server: {}, // { name: ###, uri: ### }
+  selected_library: {}, // { name: ###, id: ### }
+  server_options: [], // list of plex server connections viable in settings
+  library_options: [], // list of libraries for selected server
   inputText: '',
   foundShows: [],
   seasons: [],
