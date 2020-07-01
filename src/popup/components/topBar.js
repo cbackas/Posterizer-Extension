e879e15e516ef1c517412c4fd1ec52b6475e2c56
Zac Gibson <zgibson@live.com>
2020-07-01 16:39:02 -0500

    add library selection

--- src/popup/components/topBar.js
+++ src/popup/components/topBar.js
@@ -1,14 +1,44 @@
 import React from 'react';
-import { view } from '@risingstack/react-easy-state';
-import { Grid, Button, Header } from 'semantic-ui-react';
+import * as browser from 'webextension-polyfill';
+import { view, batch } from '@risingstack/react-easy-state';
+import { Grid, Button, Header, Dropdown } from 'semantic-ui-react';
 
 import dataStore from '../dataStore';
+
+const plex = require('../../lib/js/plex');
 
 // this is all contained inside each plex show on the search list
 class TopBar extends React.Component {
   constructor(props) {
     super(props);
+
+    plex.getSelectedServerLibraries().then((res, rej) => {
+      if (res) {
+        dataStore.library_options = res;
+      }
+      if (rej) dataStore.library_options = [];
+    });
   }
+
+  handleDropdownClick = (e, { value }) => {
+    dataStore.selected_library.id = value;
+
+    const { library_options } = dataStore;
+    const { text: name } = library_options.find(
+      ({ value: val }) => val === value
+    );
+
+    batch(() => {
+      dataStore.selected_library.name = name;
+      dataStore.selected_library.id = value;
+    });
+    // set browser storage selected_lib_id to null if 'none' was selected
+    browser.storage.local.set({
+      selected_lib_id: value === 'none' ? null : value,
+      selected_lib_name: name
+    });
+    console.log(`Set selected_library: ${name} - ${value}`);
+  };
 
   handleSettingsCog = () => {
     const { settings_view } = dataStore;
@@ -16,7 +46,12 @@ class TopBar extends React.Component {
   };
 
   render() {
-    const { settings_view } = dataStore;
+    const {
+      settings_view,
+      selected_server,
+      selected_library,
+      library_options
+    } = dataStore;
     return (
       <React.Fragment>
         <Grid
@@ -28,10 +63,33 @@ class TopBar extends React.Component {
             backgroundColor: '#FFF'
           }}
         >
-          <Grid.Column style={{ width: '75%' }}>
-            <Header size="large">Posterizer</Header>
+          <Grid.Column style={{ width: '30%' }}>
+            <Header size="large" style={{ margin: 0 }}>
+              Posterizer
+            </Header>
           </Grid.Column>
-          <Grid.Column style={{ width: '25%', textAlign: 'right' }}>
+          <Grid.Column style={{ width: '50%' }}>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'horizontal'
+              }}
+            >
+              <h4 style={{ margin: 0 }}>Plex Server:&nbsp;</h4>
+              {selected_server.name}
+            </div>
+
+            <Dropdown
+              placeholder="select library..."
+              selection
+              value={selected_library.id}
+              onChange={this.handleDropdownClick}
+              options={library_options}
+            />
+          </Grid.Column>
+          <Grid.Column
+            style={{ width: '20%', textAlign: 'center', padding: '1.5em' }}
+          >
             <Button
               icon="cog"
               active={settings_view}
