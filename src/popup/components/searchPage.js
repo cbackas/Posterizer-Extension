9e05e3f61b5370d83404809edba8d82c3cade4f4
Zac Gibson <zgibson@live.com>
2020-07-01 16:38:27 -0500

    convert to class comp and notes about search size limits

--- src/popup/components/searchPage.js
+++ src/popup/components/searchPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { view } from '@risingstack/react-easy-state';
+import { view, store } from '@risingstack/react-easy-state';
 import _ from 'lodash';
 import { Input, Button, Segment, Grid, Header } from 'semantic-ui-react';
 
@@ -8,77 +8,92 @@ import DimmerImage from './dimmerImage';
 
 const plex = require('../../lib/js/plex');
 
-function SearchPage() {
-  const handleInput = event => (dataStore.inputText = event.target.value);
+class SearchPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-  // eslint-disable-next-line no-unused-vars
-  const handleSearch = (event, data) => {
-    if (dataStore.inputText !== '') {
-      plex.searchShows();
-    }
+    this.compStore = store({
+      input_text: ''
+    });
+  }
+
+  handleInput = event => (this.compStore.input_text = event.target.value);
+
+  handleSearch = () => {
+    const { input_text } = this.compStore;
+    plex.searchShows(input_text);
   };
 
-  const handleKeyPress = event => {
+  handleKeyPress = event => {
     if (event.key === 'Enter') {
-      handleSearch();
+      this.handleSearch();
     }
   };
+  render() {
+    const { foundShows } = dataStore;
+    const { input_text } = this.compStore;
 
-  return (
-    <React.Fragment>
-      <Segment
-        style={{
-          overflow: 'auto',
-          position: 'absolute',
-          top: '5.5em',
-          width: '90%'
-        }}
-        raised
-      >
-        <div>
-          <Input
-            placeholder="Search for a show..."
-            value={dataStore.inputText}
-            onChange={handleInput}
-            onKeyPress={handleKeyPress}
-            style={{ width: '70%' }}
-          />
-          <Button
-            onClick={handleSearch}
-            content="Search"
-            style={{ width: '25%' }}
-          />
-        </div>
-      </Segment>
-      <Grid
-        centered
-        relaxed
-        columns={3}
-        style={{
-          margin: '6em 1.5em 4em 1.5em',
-          minHeight: '350px',
-          height: '100%'
-        }}
-      >
-        {dataStore.foundShows.map(show => (
-          <Grid.Column key={show.ratingKey} style={{ height: 300, width: 200 }}>
-            <Segment raised style={{ height: '100%', width: '100%' }}>
-              <DimmerImage show={_.cloneDeep(show)} />
-              <div
-                style={{
-                  paddingTop: '10px',
-                  paddingBottom: '10px',
-                  textAlign: 'center'
-                }}
-              >
-                <Header as="h4">{show.title}</Header>
-              </div>
-            </Segment>
-          </Grid.Column>
-        ))}
-      </Grid>
-    </React.Fragment>
-  );
+    return (
+      <React.Fragment>
+        <Segment
+          style={{
+            overflow: 'auto',
+            position: 'absolute',
+            top: '6.5em',
+            width: '90%'
+          }}
+          raised
+        >
+          <div>
+            <Input
+              placeholder="Search for a show..."
+              value={input_text}
+              onChange={this.handleInput}
+              onKeyPress={this.handleKeyPress}
+              style={{ width: '70%' }}
+            />
+            <Button
+              onClick={this.handleSearch}
+              content="Search"
+              style={{ width: '25%' }}
+            />
+          </div>
+        </Segment>
+        <Grid
+          centered
+          relaxed
+          columns={3}
+          style={{
+            margin: '6em 1.5em 4em 1.5em',
+            minHeight: '350px',
+            height: '100%'
+          }}
+        >
+          {foundShows.map(show => (
+            <Grid.Column key={show.id} style={{ height: 300, width: 200 }}>
+              <Segment raised style={{ height: '100%', width: '100%' }}>
+                <DimmerImage show={_.cloneDeep(show)} />
+                <div
+                  style={{
+                    paddingTop: '10px',
+                    paddingBottom: '10px',
+                    textAlign: 'center'
+                  }}
+                >
+                  <Header as="h4">{show.name}</Header>
+                </div>
+              </Segment>
+            </Grid.Column>
+          ))}
+          {foundShows.length >= 30 ? (
+            <Header size="small" style={{ color: 'white' }}>
+              30 items shown. To see more, use a better query.
+            </Header>
+          ) : null}
+        </Grid>
+      </React.Fragment>
+    );
+  }
 }
 
 export default view(SearchPage);
